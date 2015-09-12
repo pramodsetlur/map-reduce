c613d07cd8a660eae2e1c317b1bc229aa940fb31
Pramod Setlur <pramod.setlur@ticketmaster.com>
2015-09-11 17:05:56 -0700

    tf-df working. Order not maintained

--- pramod_setlur_tf_df.py
+++ pramod_setlur_tf_df.py
@@ -16,26 +16,30 @@ def mapper(record):
     # value: document contents
     document_id = record[0]
     text = record[1]
-    regex = r'\w+'
+    regex = '\w+'
     words = re.findall(regex, text)
-    #print words
     for w in words:
         #Create a dictionary with {document_id, 1} and pass that as the value
-        #print w
         dict_tf = {document_id: 1}
-        mr.emit_intermediate(w, dict_tf)
+        mr.emit_intermediate(str(w), dict_tf)
 
 def reducer(key, dict_values):
     # key: word
-    # value: list of occurrence counts
+    # dict_values: dictionary of document_id, 1 for each occurance of the string in the document
     dict_documents =  {}
-    for document_id, tf in dict_values.iteritems():
-        dict_documents.setdefault(document_id, 0)
-        old_tf = dict_documents[document_id]
-        dict_documents[document_id] = old_tf + 1
+    for doc_tf_tuple in dict_values:
+        for document_id, tf in doc_tf_tuple.iteritems():
+            dict_documents.setdefault(document_id, 0)
+            old_tf = dict_documents[document_id]
+            dict_documents[document_id] = old_tf + 1
+
+    list_documents = []
+    for document_id, tf in dict_documents.iteritems():
+        doc_tf_pair = [document_id, tf]
+        list_documents.append(doc_tf_pair)
+
     df = len(dict_documents)
-    value = [df, dict_documents]
-    mr.emit(key, value)
+    mr.emit((key,df, list_documents))
 
 # Do not modify below this line
 # =============================
